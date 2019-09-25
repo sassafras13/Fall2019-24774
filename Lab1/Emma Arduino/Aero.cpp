913386ab968d6c5ef7a680568e83b0eabd615bb0
Emma <34921873+sassafras13@users.noreply.github.com>
2019-09-25 16:08:27 -0400

    Add Emma's Arduino code

---
+++ Lab1/Emma Arduino/Aero.cpp
@@ -0,0 +1,130 @@
+/*
+Quanser Aero Core Library
+
+Created 2016 by Quanser Inc.
+www.quanser.com
+*/
+
+#include <Arduino.h>
+#include "Aero.h"
+
+//Status Bit Masks
+const byte motor1StallErrorMask = B00100000;
+const byte motor1StallDetectedMask = B00010000;
+const byte motor0StallErrorMask = B00001000;
+const byte motor0StallDetectedMask = B00000100;
+const byte amplifier1FaultMask = B00000010;
+const byte amplifier0FaultMask = B00000001;
+
+//Serial Variables
+static String dData;  // string that will be printed to the Arduino Serial Monitor
+static boolean dDataReady;  // true when there is a string ready to be printed
+static int dDataIndex;  // used to print the string one character at a time
+
+//Create the display class
+Display::Display()
+{
+	this->dData = "";
+	this->dDataReady = false;
+	this->dDataIndex = 0;
+
+	// reserve 256 bytes for the string that will be printed to the Arduino Serial Monitor
+	dData.reserve(256);
+}
+
+
+//void Display::buildString(float encoder0Deg, float encoder1Deg, float encoder2Deg, float encoder3Deg, float currentSense0, float currentSense1, int baseModuleID, int coreModuleID, int moduleStatus)
+ void Display::buildString(float encoder2Deg, float encoder3Deg, float currentSense0, float currentSense1)
+//void Display::buildString(float currentSense0, float currentSense1)
+{
+
+	if (this->dDataReady == false) {
+		// assemble the string to be printed to the Arduino Serial Monitor
+		// (Note that the String() conversion function is time consuming.  If more data
+		// needs to displayed than in this example, it may be necessary to assemble the
+		// string over multiple sample periods.)
+
+	//	reset(baseModuleID, coreModuleID, moduleStatus);
+
+//    this->dData += "\r\nM0 (deg): ";
+//    this->dData += String(round(encoder0Deg));
+//
+//    this->dData += "\r\nM1 (deg): ";
+//   this->dData += String(round(encoder1Deg));
+
+		this->dData += "\r\nPitch(deg): ";
+		this->dData += String(round(encoder2Deg));
+
+		this->dData += "\r\nYaw(deg): ";
+		this->dData += String(round(encoder3Deg));
+
+    this->dData += "\r\nM0 I(mA): ";
+    float currentSense0Amps = (3.3 / (4.0 * 4095.0)) * ((currentSense0 / 2.0) - 4095.0);
+    this->dData += String(currentSense0Amps * 1000);
+
+		this->dData += "\r\nM1 I(mA): ";
+		float currentSense1Amps = (3.3 / (4.0 * 4095.0)) * ((currentSense1 / 2.0) - 4095.0);
+		this->dData += String(currentSense1Amps * 1000);
+ //  this->dData += String(seconds);      // ===> just for testing seconds 
+		this->dData += "\r\n\n";
+
+		this->dDataReady = true;  // the string is ready to be printed
+		this->dDataIndex = 0;
+	}
+}
+
+void Display::reset(int baseModuleID, int coreModuleID, int moduleStatus)
+{
+	this->dData = "";  // clear the string
+
+	if (baseModuleID == -1) {
+    this->dData += "Base Module ID: No module detected";
+  }
+  else {
+    this->dData += "Base Module ID: ";
+    this->dData += String(baseModuleID);
+  }
+
+  if (coreModuleID == -1) {
+		this->dData += "\r\nCore Module ID: No module detected";
+	}
+	else {
+		this->dData += "\r\nCore Module ID: ";
+		this->dData += String(coreModuleID);
+	}
+
+  // moduleStatus is 0 when there are no errors
+  if (moduleStatus == 0) {
+    this->dData += "\r\nStatus: Good";  // '\r' is carriage return and '\n' is new line
+  }
+  // check for stalls
+  else if (moduleStatus & (motor0StallErrorMask | motor0StallDetectedMask | motor1StallErrorMask | motor1StallDetectedMask)) {
+    if (moduleStatus & motor0StallErrorMask) {
+      this->dData += "\r\nStatus: Motor 0 Stall Error";
+    }
+    else if (moduleStatus & motor0StallDetectedMask) {
+      this->dData += "\r\nStatus: Motor 0 Stall Detected";
+    }
+    if (moduleStatus & motor1StallErrorMask) {
+      this->dData += "\r\nStatus: Motor 1 Stall Error";
+    }
+    else if (moduleStatus & motor1StallDetectedMask) {
+      this->dData += "\r\nStatus: Motor 1 Stall Detected";
+    }
+  }
+  
+  // amplifier faults 
+  else if (moduleStatus & (amplifier0FaultMask | amplifier1FaultMask)) {
+    if (moduleStatus & amplifier0FaultMask) {
+      this->dData += "\r\nStatus: Amplifier 0 Fault";
+    }
+    if (moduleStatus & amplifier1FaultMask) {
+      this->dData += "\r\nStatus: Amplifier 1 Fault";
+    }
+  }
+  // if moduleStatus is an unexpected value, print the value
+	else {
+		this->dData += "\r\nStatus: ";
+		this->dData += String(moduleStatus);
+	}  
+}
---
+++ Lab1/Emma Arduino/Functions.ino
@@ -0,0 +1,192 @@
+////
+//
+//void AeroGains()
+//{
+//  //Calculates voltage to be applied to Front and Back Motors K*u
+//float OutP=0;
+//float OutY=0;
+//for (int it=0; it<4;it++){
+//
+//OutP=OutP+Error[it]*gainP[it];
+//OutY=OutY+Error[it]*gainY[it];
+//}
+//  Vmotors[0]=OutP;
+//  Vmotors[1]=OutY;
+//}
+
+//
+//
+//void SetpointGen_Team4()
+//{
+//  if (milisecs>=1000){secs++; milisecs=0;Serial.println(secs);}
+//  // check that pitchOn variable is true
+//  if (pitchOn == 1){
+//     desired[1] = 0.5236 ; // set value of pitch reference signal HIGH
+//     LEDRed = 0;
+//     LEDGreen = 0;
+//     LEDBlue = 999 ; 
+//     // check to see if we need to update pitchOn
+//     if (secs%pitchRate == 0){
+//      Serial.println("Pitch ON") ; 
+//      Serial.println(secs%pitchRate) ; 
+//      Serial.println(pitchOn) ; 
+//      pitchOn = 0; // if the time is a multiple of the pitchRate then change the value
+//     }
+//  }
+//  if (pitchOn == 0){
+//    desired[1] = -0.5236 ; // set value of pitch reference signal LOW
+//    LEDRed = 0;
+//    LEDGreen = 999;
+//    LEDBlue = 0;
+//    // check to see if we need to update pitchOn
+//    if (secs%pitchRate == 0){
+//      Serial.println("Pitch OFF") ; 
+//      Serial.println(secs%pitchRate) ; 
+//      Serial.println(pitchOn) ; 
+//      pitchOn = 1; // if the time is a multiple of the pitchRate then change the value
+//    }
+//  }
+//  // can I have two if statements back to back?
+//  // check that yawOn variable is true
+//  if (yawOn == 1){
+//     desired[0] = 0.7845; // set value of yaw reference signal HIGH
+//     // check to see if we need to update yawOn
+//     if (secs%yawRate == 0){
+//      yawOn = 0; // if the time is a multiple of the yawRate then change the value
+//     }
+//  }
+//  if (yawOn == 0){
+//    desired[0] = -0.7854; // set value of yaw reference signal LOW
+//    // check to see if we need to update yawOn
+//    if (secs%yawRate == 0){
+//      yawOn = 1; // if the time is a multiple of the yawRate then change the value
+//    }
+//  }
+//  return;
+//}
+void SetpointGen_Team4()
+{
+  Serial.println("updating clock") ; 
+  Serial.print("Milisecs: "); Serial.println(milisecs) ; 
+  if (milisecs>=500){
+    secs++; 
+    secs2++; 
+    milisecs=0;
+    Serial.print("!!!SECONDS (pitch):"); Serial.println(secs);
+    Serial.print("!!!SECONDS (yaw):");Serial.println(secs2); 
+  }
+  // --------------------- PITCH ---------------------------------
+  if (secs>=pitchRate && secs<(pitchRate+1)){
+    if (pitchOn == 0){
+      Serial.println("!!!CHANGE PITCH RATE TO 1!!!") ; 
+      //desired[0] = 0.5236 ; // set value of pitch reference signal HIGH
+      desired[0] = 0 ; 
+      LEDRed = 0;
+      LEDGreen = 0;
+      LEDBlue = 999 ; 
+      pitchOn = 1 ; 
+      secs = 0 ; 
+      return ; 
+    }
+    else {
+      Serial.println("!!!CHANGE PITCH RATE TO 0!!!") ; 
+      //desired[0] = -0.5236 ; // set value of pitch reference signal LOW
+      desired[0] = 0 ; 
+      LEDRed = 0;
+      LEDGreen = 999;
+      LEDBlue = 0;
+      pitchOn = 0 ; 
+      secs = 0 ; 
+      return ; 
+    }
+  }
+  // ----------------------- YAW -----------------------------
+  if (secs2>=yawRate && secs2<(yawRate+1)){
+  if (yawOn == 0){
+    Serial.println("!!!CHANGE YAW RATE TO 1!!!") ; 
+    //desired[1] = 0.7845; // set value of yaw reference signal HIGH    
+    desired[1] = 0 ; 
+//    LEDRed = 0;
+//    LEDGreen = 0;
+//    LEDBlue = 999 ; 
+    yawOn = 1 ; 
+    secs2 = 0 ; 
+    return ; 
+  }
+  else {
+    Serial.println("!!!CHANGE YAW RATE TO 0!!!") ; 
+    //desired[1] = -0.7845 ; // set value of yaw reference signal LOW
+    desired[1] = 0 ; 
+//    LEDRed = 0;
+//    LEDGreen = 999;
+//    LEDBlue = 0;
+    yawOn = 0 ; 
+    secs2 = 0 ; 
+    return ; 
+  }
+ }
+}
+//
+void SetpointGen()
+{
+  //  Generates setpoints for Pitch and Yaw angles at specific times
+  // Note that this is not Real-Time control. The units called seconds should be renamed..
+  //
+    if (milisecs>=1000){seconds++; milisecs=0;}
+     if (seconds >= 4 && seconds <8 ){
+      desired[0] = 0.35;
+       desired[1] = 0;
+      LEDRed = 0;
+      LEDGreen = 0;
+      LEDBlue = 999;
+      return;
+      }
+      
+     if (seconds >= 8 && seconds <12){
+       desired[0] = -0.35;
+       desired[1] = 0;
+      LEDRed = 999;
+      LEDGreen = 500;
+      LEDBlue = 0;
+      return;
+      }
+ if (seconds >=12 && seconds <16){
+      desired[0] = 0;
+      desired[0] = 0;
+      LEDRed = 0;
+      LEDGreen = 0;
+      LEDBlue = 999;
+      return;
+      }
+
+      if (seconds >= 16 && seconds <20 ){
+      desired[0] = 0;
+       desired[1] = 0.4;
+      LEDRed = 999;
+      LEDGreen = 500;
+      LEDBlue = 0;
+      return;
+      }
+      
+     if (seconds >= 20 && seconds <24){
+      //seconds=0;
+       desired[0] = 0;
+       desired[1] = -0.4;
+      LEDRed = 0;
+      LEDGreen = 0;
+      LEDBlue = 999;
+      return;
+      }
+        if (seconds>=24){ 
+       seconds=0;
+       desired[0] = 0;
+       desired[1] = 0;
+      LEDRed = 999;
+      LEDGreen = 500;
+      LEDBlue = 0;
+      return;
+      }
+      
+       
+      
+}
