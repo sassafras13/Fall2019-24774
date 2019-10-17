2f6c7f6846a853e6d7651a4b80c330778c644664
ke1mura <42795052+ke1mura@users.noreply.github.com>
2019-10-17 15:29:08 -0400

    Add files via upload

---
+++ QUBEServo.h
@@ -0,0 +1,22 @@
+/*
+Quanser QUBE Servo Header
+
+Created 2016 by Quanser Inc.
+www.quanser.com
+*/
+
+#ifndef QUBEServo_h
+#define QUBEServo_h
+
+class Display
+{
+public:
+	Display(); //Build the display class
+	void buildString(float theta, float alpha, float currentSense, int moduleID, int moduleStatus); //Create the data string
+	String dData;  // string that will be printed to the Arduino Serial Monitor
+	boolean dDataReady;  // true when there is a string ready to be printed
+	int dDataIndex;  // used to print the string one character at a time
+private:
+	void reset(int moduleID, int moduleStatus); //reset the data string
+};
+#endif
