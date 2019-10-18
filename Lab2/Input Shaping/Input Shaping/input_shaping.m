be7bc96ccb3582bb040db7f5d12551541ecc7f40
Pranav Narahari <44911052+pranav-narahari@users.noreply.github.com>
2019-10-18 04:17:07 -0400

    Input shaping Matlab files

---
+++ Lab2/Input Shaping/Input Shaping/input_shaping.m
@@ -0,0 +1,82 @@
+wn = 10;
+zeta = 0.01;
+K = exp(-zeta*pi/sqrt(1-zeta^2));
+A0 = 1/(1+2*K+K^2);
+A1 = 2*K/(1+2*K+K^2);
+A2 = K^2/(1+2*K+K^2);
+t1 = pi/wn/sqrt(1-zeta^2);
+t2 = 2*pi/wn/sqrt(1-zeta^2);
+
+% n=4;
+% d = log(1.7/1.4)/n;
+% 
+% z = d/sqrt(2*pi^2 + d^2);
+
+%%
+%wn 1.2
+wn = 10;
+wn = 1.2*wn;
+zeta = 0.01;
+K = exp(-zeta*pi/sqrt(1-zeta^2));
+A0 = 1/(1+2*K+K^2);
+A1 = 2*K/(1+2*K+K^2);
+A2 = K^2/(1+2*K+K^2);
+t1 = pi/wn/sqrt(1-zeta^2);
+t2 = 2*pi/wn/sqrt(1-zeta^2);
+%%
+%wn 1.5
+wn = 10;
+wn = 1.5*wn;
+zeta = 0.01;
+K = exp(-zeta*pi/sqrt(1-zeta^2));
+A0 = 1/(1+2*K+K^2);
+A1 = 2*K/(1+2*K+K^2);
+A2 = K^2/(1+2*K+K^2);
+t1 = pi/wn/sqrt(1-zeta^2);
+t2 = 2*pi/wn/sqrt(1-zeta^2);
+%%
+%wn 2
+wn = 10;
+wn = 2*wn;
+zeta = 0.01;
+K = exp(-zeta*pi/sqrt(1-zeta^2));
+A0 = 1/(1+2*K+K^2);
+A1 = 2*K/(1+2*K+K^2);
+A2 = K^2/(1+2*K+K^2);
+t1 = pi/wn/sqrt(1-zeta^2);
+t2 = 2*pi/wn/sqrt(1-zeta^2);
+
+%%
+%zeta 1.2
+zeta = 0.01;
+wn = 10;
+zeta = 1.2*zeta;
+K = exp(-zeta*pi/sqrt(1-zeta^2));
+A0 = 1/(1+2*K+K^2);
+A1 = 2*K/(1+2*K+K^2);
+A2 = K^2/(1+2*K+K^2);
+t1 = pi/wn/sqrt(1-zeta^2);
+t2 = 2*pi/wn/sqrt(1-zeta^2);
+%%
+%zeta 1.5
+zeta = 0.01;
+wn = 10;
+zeta = 1.5*zeta;
+K = exp(-zeta*pi/sqrt(1-zeta^2));
+A0 = 1/(1+2*K+K^2);
+A1 = 2*K/(1+2*K+K^2);
+A2 = K^2/(1+2*K+K^2);
+t1 = pi/wn/sqrt(1-zeta^2);
+t2 = 2*pi/wn/sqrt(1-zeta^2);
+%%
+%zeta 2
+zeta = 0.01;
+wn = 10;
+zeta = 2*zeta;
+K = exp(-zeta*pi/sqrt(1-zeta^2));
+A0 = 1/(1+2*K+K^2);
+A1 = 2*K/(1+2*K+K^2);
+A2 = K^2/(1+2*K+K^2);
+t1 = pi/wn/sqrt(1-zeta^2);
+t2 = 2*pi/wn/sqrt(1-zeta^2);
+
